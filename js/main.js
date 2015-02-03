cfa5bdeeb7c4753949f91eaaa3e79ba36a565be3
Michael Angelo <yamikamisama@gmail.com>
2015-02-02 22:13:15 -0500

    add breakpoints for carousel

--- js/main.js
+++ js/main.js
@@ -5,7 +5,23 @@ $(document).ready(function() {
       navigation : true, // Show next and prev buttons
       slideSpeed : 300,
       paginationSpeed : 400,
-      singleItem:true
+      singleItem:true,
+      responsiveClass:true,
+      responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:1,
+            nav:false
+        },
+        1000:{
+            items:1,
+            nav:true,
+            loop:false
+        }
+    }
 
       // "singleItem:true" is a shortcut for:
       // items : 1,
@@ -16,19 +32,19 @@ $(document).ready(function() {
 
   });
   $("#bio_link").click(function(){
-    $("#center_carousel").children().hide();
+    $("#center_view").children().hide();
     $("#bio").fadeIn(1000);
   });
   $("#music_link").click(function(){
-    $("#center_carousel").children().hide();
+    $("#center_view").children().hide();
     $("#music").fadeIn(1000);
   });
    $("#video_link").click(function(){
-    $("#center_carousel").children().hide();
+    $("#center_view").children().hide();
     $("#video").fadeIn(1000);
   });
     $("#pictures_link").click(function(){
-    $("#center_carousel").children().hide();
+    $("#center_view").children().hide();
     $("#owl-demo").fadeIn(1000);
   });
 });
