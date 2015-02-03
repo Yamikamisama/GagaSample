f042a7b5df6da364cd418f786ef544a5f7ac3bb9
Michael Angelo <yamikamisama@gmail.com>
2015-02-03 02:20:26 -0500

    mobile view created

--- public/js/main.js
+++ public/js/main.js
@@ -27,4 +27,22 @@ $(document).ready(function() {
     $("#center_view").children().hide();
     $("#owl-demo").fadeIn(1000);
   });
+
+  $(function() {
+      var pull    = $('#pull');
+        menu    = $('nav ul');
+        menuHeight  = menu.height();
+
+      $(pull).on('click', function(e) {
+        e.preventDefault();
+        menu.slideToggle();
+      });
+
+      $(window).resize(function(){
+            var w = $(window).width();
+            if(w > 320 && menu.is(':hidden')) {
+              menu.removeAttr('style');
+            }
+        });
+    });
 });
