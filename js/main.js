113fbde61e97504113b8c89543747f77610801da
Michael Angelo <yamikamisama@gmail.com>
2015-02-02 22:54:51 -0500

    add php page to launch to heroku

--- js/main.js
+++ js/main.js
@@ -5,45 +5,25 @@ $(document).ready(function() {
       navigation : true, // Show next and prev buttons
       slideSpeed : 300,
       paginationSpeed : 400,
-      singleItem:true,
-      responsiveClass:true,
-      responsive:{
-        0:{
-            items:1,
-            nav:true
-        },
-        600:{
-            items:1,
-            nav:false
-        },
-        1000:{
-            items:1,
-            nav:true,
-            loop:false
-        }
-    }
-
-      // "singleItem:true" is a shortcut for:
-      // items : 1,
-      // itemsDesktop : false,
-      // itemsDesktopSmall : false,
-      // itemsTablet: false,
-      // itemsMobile : false
-
+      singleItem:true
   });
+
   $("#bio_link").click(function(){
     $("#center_view").children().hide();
     $("#bio").fadeIn(1000);
   });
+
   $("#music_link").click(function(){
     $("#center_view").children().hide();
     $("#music").fadeIn(1000);
   });
-   $("#video_link").click(function(){
+
+  $("#video_link").click(function(){
     $("#center_view").children().hide();
     $("#video").fadeIn(1000);
   });
-    $("#pictures_link").click(function(){
+
+  $("#pictures_link").click(function(){
     $("#center_view").children().hide();
     $("#owl-demo").fadeIn(1000);
   });
