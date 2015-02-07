723d91eb60c95912eec3f9856fe31cb4bede5eed
Michael Angelo <yamikamisama@gmail.com>
2015-02-07 17:44:59 -0500

    carousel fixed

--- public/js/application.js
+++ public/js/application.js
@@ -1,60 +1,39 @@
 $(document).ready(function() {
 
 
-    //carousel
-
-    var change_img_time = 4000,
-        transition_speed = 400;
-
+    //Carousel How it works
     var listItems = $("#items").children('li'),
         activeImageItems = $('#activeImages').children('li'),
-        listLen = listItems.length,
+        listLength = listItems.length,
         current,
         changeTimeout;
 
-    function moveTo(newIndex) {
+    function move(newIndex) {
 
-        var i = newIndex;
-
-        if (newIndex == 'prev') {
-            i = (current > 0) ? (current - 1) : (listLen - 1);
-        }
-
-        if (newIndex == 'next') {
-            i = (current < listLen - 1) ? (current + 1) : 0;
-        }
+        var i = (current < listLength - 1) ? (current + 1) : 0;
 
         activeImageItems.removeClass('active')
             .eq(i).addClass('active');
 
-        listItems.fadeOut(transition_speed)
-            .eq(i).fadeIn(transition_speed);
+        listItems.fadeOut(400)
+            .eq(i).fadeIn(400);
 
         current = i;
 
-        //resets time interval if user clicks on slider activeImage; then begin automated slider
         clearTimeout(changeTimeout);
         changeTimeout = setTimeout(function() {
-            moveTo('next');
-        }, change_img_time);
+            move('next');
+        }, 4000);
     };
 
-    // Event handlers
+    // Allow circle to be clicked
     $("#activeImages li").click(function() {
         var i = $('#activeImages li').index(this);
-        moveTo(i);
+        move(i);
     });
 
-    $("#prev").click(function() {
-        moveTo('prev');
-    });
-
-    $("#next").click(function() {
-        moveTo('next');
-    });
-
-    //initialize slider on load
-    moveTo('next');
+    //Start Carousel
+    move('next');
 
 
 
@@ -65,7 +44,7 @@ $(document).ready(function() {
     // var  xmlhttp = new XMLHttpRequest();
     // var url = "https://api.twitter.com/1.1/statuses/user_timeline.json";
     // xmlhttp.open('GET',url,true);
-    // xmlhttp.setRequestHeader('Authorization','OAuth oauth_consumer_key="HdFdA3C3pzTBzbHvPMPw", oauth_nonce="4148fa6e3dca3c3d22a8315dfb4ea5bb", oauth_signature="uDZP2scUz6FUKwFie4FtCtJfdNE%3D", oauth_signature_method="HMAC-SHA1", oauth_timestamp= "1359955650", oauth_token, "1127121421-aPHZHQ5BCUoqfHER2UYhQYUEm0zPEMr9xJYizXl", oauth_version="1.0"');
+    // xhr.setRequestHeader('Authorization','OAuth oauth_consumer_key="HIDDEN", oauth_nonce="HIDDEN", oauth_signature="HIDDEN", oauth_signature_method="HIDDEN", oauth_timestamp= "HIDDEN", oauth_token, "HIDDEN", oauth_version="1.0"');
     // xmlhttp.send();
 
 
@@ -75,7 +54,7 @@ $(document).ready(function() {
     //     xhr.open(method, url, true);
     //     xhr.setRequestHeader('Access-Control-Allow-Origin', "http://localhost:9292");
     //     xhr.withCredentials = true;
-    //     xhr.setRequestHeader('Authorization','OAuth oauth_consumer_key="HdFdA3C3pzTBzbHvPMPw", oauth_nonce="4148fa6e3dca3c3d22a8315dfb4ea5bb", oauth_signature="uDZP2scUz6FUKwFie4FtCtJfdNE%3D", oauth_signature_method="HMAC-SHA1", oauth_timestamp= "1359955650", oauth_token, "1127121421-aPHZHQ5BCUoqfHER2UYhQYUEm0zPEMr9xJYizXl", oauth_version="1.0"');
+    //     xhr.setRequestHeader('Authorization','OAuth oauth_consumer_key="HIDDEN", oauth_nonce="HIDDEN", oauth_signature="HIDDEN", oauth_signature_method="HIDDEN", oauth_timestamp= "HIDDEN", oauth_token, "HIDDEN", oauth_version="1.0"');
     //     xhr.send()
     //   } else {
     //     // CORS not supported.
